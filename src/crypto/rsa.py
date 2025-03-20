525f91431cac1fe8aada90ad63c775b14a81f0ca
GabyIsBored <79170829+GabyIsBored@users.noreply.github.com>
2025-03-20 09:51:14 +0100

    Update rsa.py

--- src/crypto/rsa.py
+++ src/crypto/rsa.py
@@ -2,9 +2,14 @@
 # https://www.askpython.com/python/examples/rsa-algorithm-in-python
 # https://youtu.be/wcbH4t5SJpg
 # https://youtu.be/qdylJqXCDGs, http://stackoverflow.com/questions/6325576/how-many-iterations-of-rabin-miller-should-i-use-for-cryptographic-safe-primes
+
+# character size indication
+# chinese remainder theorem
+
 import random
 
 size=1024
+message=r"""Sure! Let's dive deeper into the mathematics behind each function in the program. This will help clarify how the operations work, especially in the context of cryptography, particularly RSA-like encryption. ◌󠇯"""
 
 def gcd(a, b):
     while a > 0:
@@ -12,17 +17,20 @@ def gcd(a, b):
         (a, b) = (b, a)
     return b
 
-"""
-def modInverse(A, M):
-    if gcd(A, M) > 1:
-        # modulo inverse does not exist
-        return -1
-    for X in range(1, M):
-        print("e")
-        if ((A % M) * (X % M)) % M == 1:
-            return X
-    return -1
-"""
+def mod_exp(base, power, mod):
+    result = 1
+    while power > 0:
+        # If power is odd
+        if power % 2 == 1:
+            result = (result * base) % mod
+
+        # Divide the power by 2
+        power = power // 2
+        # Multiply base to itself
+        base = (base * base) % mod
+
+    return result
+
 def miller_rabin(n):
     if n == 2:
         return True
@@ -80,23 +88,25 @@ d = y = pow(e, -1, r)
 
 private_key = (d, n)
 public_key = (e, n)
-print(private_key)
+
 
 def encrypt(message_input, public_key):
     encrypted_list = []
     for char in message_input:
-        num = (ord(char) ** public_key[0]) % public_key[1]
-        encrypted_list.append(num)
+        #num = (ord(char) ** public_key[0]) % public_key[1]
+        num = mod_exp(ord(char),public_key[0],public_key[1])
+        encrypted_list.append((num))
     return encrypted_list
 
 
 def decrypt(cipher_input, private_key):
     dec_str = ""
     for num in cipher_input:
-        char = (num ** private_key[0]) % private_key[1]
+        char = mod_exp(num,private_key[0],private_key[1])
         dec_str = dec_str + chr(char)
     return dec_str
 
-encryptedEmoji=encrypt('🤩', public_key)
+
+encryptedEmoji=encrypt(message, public_key)
 print(encryptedEmoji)
-"""decrypt(encryptedEmoji,private_key)"""
+print(decrypt(encryptedEmoji,private_key))
