37b3a12344db62aa478deb6a174266a4f70a2abe
GabyIsBored <79170829+GabyIsBored@users.noreply.github.com>
2025-03-24 11:02:49 +0100

    Update generateImage.py

--- src/utils/generateImage.py
+++ src/utils/generateImage.py
@@ -1,3 +1,4 @@
+
 import PIL
 from PIL import Image
 import math
@@ -7,7 +8,7 @@ class ImageGenerator:
     def __init__(self):
         self.GLOBAL_INDEX_IMAGE = (0, 0)   # (width, height)
         self.GLOBAL_INDEX_RGB = 0
-        self.image = Image.new(mode="RGB", size=(5,5))
+        self.image = Image.new(mode="RGB", size=(10,10))
         self.WIDTH = self.image.size[0]
         self.HEIGHT = self.image.size[1]
         self.PIXEL_COUNT = self.WIDTH * self.HEIGHT
@@ -43,52 +44,82 @@ class ImageGenerator:
         res=res+str(len(dec_char))+str(incrementNum)+dec_char
         return res 
 
-    def encode_text(self, text,is_watermark): #BUG
+    def encode_text(self, text,is_watermark): #DE LA MERDE POURQUOI CA ENCODE DES 0
         dec_text=''
+
         if not is_watermark:
             for char in text:
                 dec_text+=self.dec_string(ord(char))
         else:
             dec_text=self.dec_string(text)
+        
         text_index=0
+        pixels=[]
         current_pixel=[0,0,0]
+        
         while text_index < len(dec_text):
-            if int(dec_text[text_index:text_index+3])<=255:
-                num=int(dec_text[text_index:text_index+3])
+            
+            next_3_digits=int(dec_text[text_index:text_index+3])
+            next_2_digits=int(dec_text[text_index:text_index+2])
+            
+            if next_3_digits<=255:
+                num=next_3_digits
                 text_index += 3
             else:
-                num=int(dec_text[text_index:text_index+2])
+                num=next_2_digits
                 text_index += 2
+            
             self.update_globals()
             current_pixel[self.GLOBAL_INDEX_RGB]=num
             
-            
             if current_pixel[2]!=0:
-                self.image.putpixel(self.GLOBAL_INDEX_IMAGE, tuple(current_pixel))
+                pixels.append(current_pixel)
                 current_pixel=[0,0,0]
-        self.update_globals()
-        self.image.putpixel(self.GLOBAL_INDEX_IMAGE, tuple(current_pixel))
-                
+        pixels.append(current_pixel)
+        
+        
+        return pixels      
 
     def encode_watermark(self, text, type):
         #watermark_charcount = str(len(text))
-        watermark_charcount = str(len(text))
+        watermark_charcount = str(len(text))   # ERROR IF BIGGER THAN 16777216
+        
         if type == 'DEFAULT':
             watermark=self.WATERMARK_KEY_DEFAULT+watermark_charcount
-            self.encode_text(watermark,True)
+            return self.encode_text(watermark,True)
+            
 
         elif type == 'ENCODE':
             watermark=self.WATERMARK_KEY_ENCODE+watermark_charcount
-            self.encode_text(watermark,True) # TODO it might fuck up with RSA here, changes are required
+            return self.encode_text(watermark,True) # TODO it might fuck up with RSA here, changes are required
         else:
             pass
-
+    
+    def create_image(self,pixels):
+        size=math.ceil(math.sqrt(len(pixels)))
+        self.image = Image.new(mode="RGB", size=(size,size))
+        for pixel in pixels:
+            self.image.putpixel(self.GLOBAL_INDEX_IMAGE, tuple(pixel))
+            self.update_globals()
+        
+    
     def generate_image(self,text,type):
+        
+        self.set_size(text)
+        
         # encode the watermark
-        self.encode_watermark(text, type)
-
-        # encode the text
-        self.encode_text(text,False)
+        watermark_pixel_count=self.encode_watermark(text, type)
+        
+        # encode the text and save the pixel count
+        text_pixel_count=self.encode_text(text,False)
+        
+        pixels=watermark_pixel_count+text_pixel_count
+        
+        self.create_image(pixels)
+        
+        print(text_pixel_count+watermark_pixel_count-2) #PROBLEME DE 0
+        
+        
 
     '''
     # WORK IN PROGRESS           
@@ -134,12 +165,16 @@ class ImageGenerator:
 
     def decode(self):
         encoded_bits=self.get_image_text()
+        
+        
         watermark=self.read_character(encoded_bits[3:3+int(encoded_bits[1])],encoded_bits[2])
         if watermark[0] == self.WATERMARK_KEY_DEFAULT:
             type='DEFAULT'
         elif watermark[0] == self.WATERMARK_KEY_ENCODE:
             type='ENCODE'
-        char_count=int(watermark[1:9])
+            
+            
+        char_count=int(watermark[1:])
 
         image_text = ''
         current_char_count=4+int(encoded_bits[1])
@@ -149,19 +184,23 @@ class ImageGenerator:
             char_size=int(encoded_bits[current_char_count])
             char_zeroes=int(encoded_bits[current_char_count+1])
             char_string=encoded_bits[current_char_count+2:current_char_count+char_size+2]
+            
             current_character=self.read_character(char_string,char_zeroes)
             image_text+=chr(int(current_character))
+            
             current_char_count+=len(char_string)+2
             char_count-=1
+        #print(encoded_bits)
         return image_text
 
 
 if __name__ == '__main__':
     IM = ImageGenerator()
-    text = r"""gaby douaihy est le meilleur"""
+    text=r'''zinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinczinc'''
     IM.generate_image(text,'DEFAULT')
     IM.image.show()
     IM.GLOBAL_INDEX_IMAGE = (0, 0)   # (width, height)
     IM.GLOBAL_INDEX_RGB = 0
     
-    print('decoded: '+IM.decode())
+    IM.decode()
+    #print('decoded: '+IM.decode())
